897bb21d76f983ca029d5aa0f269293a63ace74b
Manuel Namici <manuel.namici@gmail.com>
2019-02-26 17:11:03 +0100

    app: add fragment for main activity

--- app/src/main/java/com/obdasystems/pocmedici/activity/MainActivity.java
+++ app/src/main/java/com/obdasystems/pocmedici/activity/MainActivity.java
@@ -1,16 +1,22 @@
 package com.obdasystems.pocmedici.activity;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.obdasystems.pocmedici.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements HomeFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        set
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
