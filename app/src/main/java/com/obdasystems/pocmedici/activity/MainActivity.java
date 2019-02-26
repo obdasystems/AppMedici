4f8c67091d6c84cfdbdba57bdcdc4b2b5a639a70
Manuel Namici <manuel.namici@gmail.com>
2019-02-26 15:19:20 +0100

    app: created home activity stub

---
+++ app/src/main/java/com/obdasystems/pocmedici/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.obdasystems.pocmedici.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.obdasystems.pocmedici.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        set
+    }
+}
